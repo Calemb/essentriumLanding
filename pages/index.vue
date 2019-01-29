0c2b40cbb64c14aeda11aadf1b48cd5bb5e270a3
sculpt0r <calemb@gmail.com>
2019-01-29 22:13:23 +0100

    Add testing UI to verify cookies

--- pages/index.vue
+++ pages/index.vue
@@ -1,37 +1,71 @@
 <template>
   <section class="container">
     <div>
-      <logo />
-      <h1 class="title">
-        essentrium-frontend
-      </h1>
-      <h2 class="subtitle">
-        Essentrium frontend
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >Documentation</a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >GitHub</a>
-      </div>
+      <p>{{ msg }}</p>
+      <br>Login:
+      <b-input v-model="login"></b-input>
+      <br>Password:
+      <b-input v-model="password" type="password"></b-input>
+      <b-button @click="Reqest('login')">Login</b-button>
+      <b-button @click="Reqest('')">INDEX</b-button>
+      <b-button @click="logoutAction">Log out</b-button>
+      <br>
+      <b-button @click="socketTestAction">Test normal socket</b-button>
+      <b-button @click="socketTestAction">Test restricted socket</b-button>
+      <nuxt-link class="nav-link" to="/news">Restricted page!</nuxt-link>
     </div>
   </section>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
+import Logo from "~/components/Logo.vue";
+import Axios from "axios";
 
 export default {
   components: {
     Logo
+  },
+  data() {
+    return {
+      login: "",
+      password: "",
+      msg: "",
+      ip: "http://127.0.0.1:3000/"
+    };
+  },
+  methods: {
+    Reqest: function(page) {
+      var that = this;
+      Axios.get(this.ip + page)
+        .then(response => {
+          that.msg = JSON.stringify(response.data) + "" + response.mail;
+          document.cookie = response.cookie
+        })
+        .catch(error => {
+          console.log(error);
+          that.msg = error;
+        })
+        .finally(() => {
+          // that.debugMsg = 'Final catch!'
+          console.log("finally ping index");
+        });
+    },
+    loginAction: function() {
+      // clk link -> send request to server:
+      // if no response or invalid auth -> don't allow redirect!
+      this.msg =
+        "send auth data to server. If they are ok -> set session data and back it here (cookie)";
+        this.Reqest('login')
+    },
+    logoutAction: function() {
+      this.msg = "log out user!";
+    },
+    socketTestAction: function() {
+      this.msg =
+        "Should test if socket response properly: allow only few requests without login, and allow much more with sockets!";
+    }
   }
-}
+};
 </script>
 
 <style>
@@ -45,8 +79,8 @@ export default {
 }
 
 .title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
+  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont,
+    "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif;
   display: block;
   font-weight: 300;
   font-size: 100px;
---
+++ pages/news/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <p>
+      User without login should't  see this page! also any axios data!
+      If come here without authentication -> send back to main page!
+    </p>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
