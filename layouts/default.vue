324e6e58189bfcd85ea27b55c9431aef53ce98d1
sculpt0r <calemb@gmail.com>
2019-04-22 21:52:40 +0200

    Frontpage on default without nuxt

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,13 +1,54 @@
 <template>
-  <div>
-    <nuxt />
+  <div id="app">
+    <div id="content">
+      <h1>essentrium</h1>
+      <h3 class="hashtags">#essentrium #tbbg #rpg #browsergame #postapo</h3>
+      <p class="sentence">Do you think, this world failed?</p>
+      <p class="sentence">You have a chance to start all again!</p>
+      <p class="sentence">Maybe the only goal is to server others? </p>
+      <p class="sentence">Serve next generation so they are not bothered with our problems?</p>
+      <a
+        href="http://how-to-dev.com/essentrium"
+        class="btn btn-dark"
+      >Dev Blog</a>
+      <div id="social">
+        <a
+          class="btn btn-dark"
+          href="https://www.instagram.com/essentrium/"
+        >Instagram</a>
+        <a
+          class="btn btn-dark"
+          href="https://twitter.com/essentrium"
+        >Twitter</a>
+        <a
+          class="btn btn-dark"
+          href="https://www.facebook.com/essentrium/"
+        >Facebook</a>
+      </div>
+    </div>
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script
+      async
+      src="https://www.googletagmanager.com/gtag/js?id=UA-137877001-1"
+    ></script>
+    <script>
+    window.dataLayer = window.dataLayer || [];
+
+    function gtag() {
+      dataLayer.push(arguments);
+    }
+    gtag("js", new Date());
+    gtag("config", "UA-137877001-1");
+    </script>
+
+    <!-- <nuxt /> -->
   </div>
 </template>
 
 <style>
 html {
-  font-family: 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI',
-    Roboto, 'Helvetica Neue', Arial, sans-serif;
+  font-family: "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI",
+    Roboto, "Helvetica Neue", Arial, sans-serif;
   font-size: 16px;
   word-spacing: 1px;
   -ms-text-size-adjust: 100%;
@@ -51,5 +92,45 @@ html {
 .button--grey:hover {
   color: #fff;
   background-color: #35495e;
+}
+#app {
+  border: 1px solid transparent;
+  width: 100vw;
+  height: 100vh;
+  background-color: #000;
+  text-align: center;
+  background-image: url("../static/bg.png");
+  background-size: auto 100%;
+  background-repeat: no-repeat;
+  background-position: center;
+}
+#content {
+  /* border: 1px solid red; */
+  margin-top: 5vh;
+  font-family: "Segoe UI", Tahoma, Geneva, Verdana, sans-serif;
+}
+h1 {
+  color: #3b8070;
+  font-size: 32pt;
+}
+.hashtags {
+  margin-bottom: 5vh;
+  background-color: rgba(1, 1, 1, 0.5);
+  font-size: 8pt;
+}
+.sentence {
+  font-size: 16pt;
+}
+a {
+  margin: 0px 1vw;
+  border: 1px groove gray;
+  margin-top: 50px;
+  font-variant: small-caps;
+}
+#social a {
+  display: inline-block;
+}
+#social {
+  margin-top: 25px;
 }
 </style>
