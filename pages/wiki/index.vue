10e95f411c4575100c68d69a83422482e0176127
sculpt0r <calemb@gmail.com>
2019-10-29 05:51:32 +0100

    Setup wiki layout

--- pages/wiki/index.vue
+++ pages/wiki/index.vue
@@ -1,5 +1,9 @@
 <template>
-
+  <div>
+    <h1>
+      Wiki
+    </h1>
+  </div>
 </template>
 
 <script>
@@ -8,6 +12,15 @@ export default {
 }
 </script>
 
-<style>
-
+<style scoped>
+h1 {
+  border-top: 1px solid gray;
+  margin-top: 3vh;
+  padding-top: 2vh;
+  /* border: 1px solid red; */
+  /* border-left:0px; */
+  /*
+  border-width: 1px;
+  border-color: red; */
+}
 </style>
